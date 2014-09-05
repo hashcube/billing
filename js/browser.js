5500403263f8ee5ca7fb635f3b5e2837a296f627
Vinod CG <vini@live.in>
2014-09-05 19:04:36 +0530

    add spaces && product data as a parameter

--- js/browser.js
+++ js/browser.js
@@ -5,29 +5,29 @@ var Billing = Class(Emitter, function (supr) {
 		FB.ui({
 			method: 'pay',
 			action: 'purchaseitem',
-			product : CONFIG.browser.products[item.type],
+			product : item.product, // URL of open graph object browser only
 			quantity: item.quantity,
 			request_id: item.reciept
 		}, this.callback);
 	};
 
 	this.callback = function (data) {
-		if(data.error_code){
-			if(typeof this.onFailure === 'function'){
+		if (data.error_code) {
+			if (typeof this.onFailure === 'function') {
 				logger.info("BILLING : onFailure of Billing plugin");
 				this.onFailure(data);
 			} else {
 				logger.info("BILLING : onFailure of Billing plugin is not defined");
 			}
-		} else if(data.status === 'completed'){
-			if(typeof this.onPurchase === 'function'){
+		} else if (data.status === 'completed') {
+			if (typeof this.onPurchase === 'function') {
 				logger.info("BILLING : < sync > onPurchase of Billing plugin");
 				this.onPurchase(data, false);
 			} else {
 				logger.info("BILLING : < sync > onPurchase of Billing plugin is not defined");
 			}
-		} else if(data.status === 'initiated'){
-			if(typeof this.onPurchase === 'function'){
+		} else if (data.status === 'initiated') {
+			if (typeof this.onPurchase === 'function') {
 				logger.info("BILLING : < async > onPurchase of Billing plugin");
 				this.onPurchase(data, true);
 			} else {
