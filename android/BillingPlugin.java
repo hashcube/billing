641185d131abaf4725b51f702c1e617c089b2e7d
sksethi25 <sksethi25@gmail.com>
2016-03-11 17:03:01 +0530

    bracing corrected

--- android/BillingPlugin.java
+++ android/BillingPlugin.java
@@ -114,8 +114,7 @@ public class BillingPlugin implements IPlugin {
 			logger.log("{billing} Entering Amazon Kindle Billing Plugin Handler");
 			try {
 				String responseCode = purchaseResponse.getRequestStatus().toString();
-				if(responseCode == "FAILED")
-				{
+				if(responseCode == "FAILED") {
 					EventQueue.pushEvent(new PurchaseEvent(null, null, "cancel"));
 					return;
 				}
