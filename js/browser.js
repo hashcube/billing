e5acfb44701cdac5a4954b90bf01ff1192489fee
Syed zainul abedin <szsimplesyed93@gmail.com>
2018-07-19 18:53:57 +0530

    Intendation fix

--- js/browser.js
+++ js/browser.js
@@ -1,43 +1,43 @@
 import event.Emitter as Emitter;
 
 var Billing = Class(Emitter, function (supr) {
-	this.purchase = function (item) {
-		FB.ui({
-			method: 'pay',
-			action: 'purchaseiap',
-			product_id: item,
-			quantity: item.quantity,
-			request_id: item.reciept
-		}, bind(this, function (data) {
-			this.callback(data, item);
-		}
-		));
-	};
+  this.purchase = function (item) {
+	FB.ui({
+	  method: 'pay',
+	  action: 'purchaseiap',
+	  product_id: item,
+	  quantity: item.quantity,
+	  request_id: item.reciept
+	}, bind(this, function (data) {
+	    this.callback(data, item);
+	  }
+	));
+  };
 
-	this.callback = function (data, item) {
-		if (!data || data.error_code) {
-			if (typeof this.onFailure === 'function') {
-				logger.info("BILLING : onFailure of Billing plugin");
-				this.onFailure(item);
-			} else {
-				logger.info("BILLING : onFailure of Billing plugin is not defined");
-			}
-		} else if (data.status === 'completed') {
-			if (typeof this.onPurchase === 'function') {
-				logger.info("BILLING : < sync > onPurchase of Billing plugin");
-				this.onPurchase(item, false);
-			} else {
-				logger.info("BILLING : < sync > onPurchase of Billing plugin is not defined");
-			}
-		} else if (data.status === 'initiated') {
-			if (typeof this.onPurchase === 'function') {
-				logger.info("BILLING : < async > onPurchase of Billing plugin");
-				this.onPurchase(item, true);
-			} else {
-				logger.info("BILLING : < async > onPurchase of Billing plugin is not defined");
-			}
+  this.callback = function (data, item) {
+    if (!data || data.error_code) {
+	  if (typeof this.onFailure === 'function') {
+	  	logger.info("BILLING : onFailure of Billing plugin");
+	  	this.onFailure(item);
+	  } else {
+	  	logger.info("BILLING : onFailure of Billing plugin is not defined");
+	  }
+    } else if (data.status === 'completed') {
+		if (typeof this.onPurchase === 'function') {
+		  logger.info("BILLING : < sync > onPurchase of Billing plugin");
+		  this.onPurchase(item, false);
+		} else {
+		  logger.info("BILLING : < sync > onPurchase of Billing plugin is not defined");
 		}
-	};
+    } else if (data.status === 'initiated') {
+	  if (typeof this.onPurchase === 'function') {
+	    logger.info("BILLING : < async > onPurchase of Billing plugin");
+	  	this.onPurchase(item, true);
+	  } else {
+	  	logger.info("BILLING : < async > onPurchase of Billing plugin is not defined");
+	  }
+	}
+  };
 });
 
 exports = new Billing();
