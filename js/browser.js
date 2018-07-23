462fa12d48a6fde24d02b26cb4fe55afb4b0939a
Syed zainul abedin <szsimplesyed93@gmail.com>
2018-07-23 17:50:17 +0530

    Handling condition where purchased item is not consumed

--- js/browser.js
+++ js/browser.js
@@ -1,6 +1,10 @@
 import event.Emitter as Emitter;
 
 var Billing = Class(Emitter, function (supr) {
+  var consumedItems = {};
+  var purchasedItems = {};
+
+
   this.purchase = function (item, access_token) {
     FB.ui({
       method: 'pay',
@@ -15,21 +19,41 @@ var Billing = Class(Emitter, function (supr) {
     if (!data || data.error_code) {
       this.onFailure(item);
     } else if (data.status === 'completed') {
+      purchasedItems[item] = 1;
       this.consumeItem(item, data.purchase_token, access_token);
     }
   };
 
   this.consumeItem = function (item, purchase_token, access_token) {
-    FB.api('/' + purchase_token + '/consume',
-      'post', {
-        access_token: access_token
-      }, bind(this, function (response) {
-        if (response && response.success) {
-          this.onPurchase(item, null, purchase_token);
-        } else {
-          this.onFailure(item);
-        }
-    }));
+    if (purchasedItems[item]) {
+      delete purchasedItems[item];
+      consumedItems[item] = {
+        token: access_token,
+        receipt: null
+      };
+      FB.api('/' + purchase_token + '/consume',
+        'post', {
+          access_token: access_token
+        }, bind(this, function (response) {
+          if (response && response.success) {
+            this.creditConsumed(item, purchase_token);
+          } else {
+            // retry consuming if failed
+            setTimeout(bind(this, function () {
+              this.creditConsumed(item, purchase_token);
+            }, 3000));
+          }
+      }));
+    }
+  };
+
+  this.creditConsumed = function (item, token) {
+    if (token) {
+      this.onPurchase(item, null, token);
+    }
+    delete consumedItems[item];
+    // saving items which were purchased but not consumed
+    localStorage.setItem("billingConsumed", JSON.stringify(consumedItems));
   };
 
   this.onPurchase = function () {};
