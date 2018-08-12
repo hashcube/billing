2776dc9e01b33aeb06471f84a2fcd7cea03c4b3b
Syed zainul abedin <szsimplesyed93@gmail.com>
2018-08-12 21:36:31 +0530

    Fixed function calling

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ var simulated_item;
 function initializeFromLocalStorage() {
   try {
     var saved = localStorage.getItem("billingConsumed"),
-      purchased_item = localStorage.removeItem("purchasedItem");
+      purchased_item = localStorage.getItem("purchasedItem");
 
     if (purchased_item) {
       browser.consumeItem(purchased_item.item,
